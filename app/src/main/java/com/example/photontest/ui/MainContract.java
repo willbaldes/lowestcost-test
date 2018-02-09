9dc15810ed8fe6029f45d3695bb6a4da3156e592
Will Baldes <willbaldes.vsc@gmail.com>
2018-02-09 02:27:13 -0500

    Add MainPresenter Test Cases

--- app/src/main/java/com/example/photontest/ui/MainContract.java
+++ app/src/main/java/com/example/photontest/ui/MainContract.java
@@ -2,14 +2,19 @@ package com.example.photontest.ui;
 
 import com.example.photontest.base.BasePresenter;
 import com.example.photontest.base.BaseView;
+import com.example.photontest.model.Result;
 
-public class MainContract {
+public interface MainContract {
 
     interface View extends BaseView {
+
+        void showResult(Result result);
 
     }
 
     interface Presenter extends BasePresenter {
+
+        void calculateLowestCost(Object[][] inputMatrix);
 
     }
 
--- app/src/test/java/com/example/photontest/MainPresenterTest.java
+++ app/src/test/java/com/example/photontest/MainPresenterTest.java
@@ -1,8 +1,47 @@
 package com.example.photontest;
 
-/**
- * Created by admin on 2/9/18.
- */
+import com.example.photontest.ui.MainPresenter;
+
+import org.junit.Before;
+import org.junit.Test;
 
 public class MainPresenterTest {
+
+    MainPresenter presenter;
+
+    @Before
+    public void init() {
+
+    }
+
+    @Test
+    public void testSuccessValidMatrix() {
+
+    }
+
+    @Test
+    public void testSuccessInvalidMatrix(){
+
+    }
+
+    @Test
+    public void testErrorInvalidInput() {
+
+    }
+
+    @Test
+    public void testErrorInvalidInputRows() {
+
+    }
+
+    @Test
+    public void testErrorInvalidInputColsLessThanMin() {
+
+    }
+
+    @Test
+    public void testErrorInvalidInputColsMoreThanMax() {
+
+    }
+
 }
